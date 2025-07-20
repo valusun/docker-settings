efaa6570dd570dc244b163c6b68acac130743e1a
valusun <chobicoco0327@gmail.com>
2025-07-20 19:25:34 +0900

    fix: comment

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -7,17 +7,17 @@ if [ -f /docker-entrypoint-initdb.d/.env ]; then
   . /docker-entrypoint-initdb.d/.env
 fi
 
-# install MongoDB packages
+# update packages
 apt-get update
 
 # replicaset
 mongosh --host mongo-primary --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} --eval "rs.initiate({
-  _id: '${MONGO_REPLICA_SET_NAME}',
+  _id: "${MONGO_REPLICA_SET_NAME}",
   members: [
-    { _id: 0, host: 'mongo-primary:${MONGO_PRIMARY_PORT}' },
-    { _id: 1, host: 'mongo-secondary1:${MONGO_SECONDARY1_PORT}' },
-    { _id: 2, host: 'mongo-secondary2:${MONGO_SECONDARY2_PORT}' },
-    { _id: 3, host: 'mongo-arbiter:${MONGO_ARBITER_PORT}', arbiterOnly: true }
+    { _id: 0, host: "mongo-primary:${MONGO_PRIMARY_PORT}" },
+    { _id: 1, host: "mongo-secondary1:${MONGO_SECONDARY1_PORT}" },
+    { _id: 2, host: "mongo-secondary2:${MONGO_SECONDARY2_PORT}" },
+    { _id: 3, host: "mongo-arbiter:${MONGO_ARBITER_PORT}", arbiterOnly: true }
   ]
 })"
 
