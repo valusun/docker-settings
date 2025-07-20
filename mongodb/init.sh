eb82139731ce632110d45624d1cb88b7431da417
valusun <chobicoco0327@gmail.com>
2025-07-20 22:29:11 +0900

    fix: sh

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -10,14 +10,14 @@ fi
 # update packages
 apt-get update
 
-# replicaset
+# replicasetい
 mongosh --host mongo-primary --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} --eval "rs.initiate({
-  _id: "${MONGO_REPLICA_SET_NAME}",
+  _id: '${MONGO_REPLICA_SET_NAME}',
   members: [
-    { _id: 0, host: "mongo-primary:${MONGO_PRIMARY_PORT}" },
-    { _id: 1, host: "mongo-secondary1:${MONGO_SECONDARY1_PORT}" },
-    { _id: 2, host: "mongo-secondary2:${MONGO_SECONDARY2_PORT}" },
-    { _id: 3, host: "mongo-arbiter:${MONGO_ARBITER_PORT}", arbiterOnly: true }
+    { _id: 0, host: 'mongo-primary:${MONGO_PRIMARY_PORT}' },
+    { _id: 1, host: 'mongo-secondary1:${MONGO_SECONDARY1_PORT}' },
+    { _id: 2, host: 'mongo-secondary2:${MONGO_SECONDARY2_PORT}' },
+    { _id: 3, host: 'mongo-arbiter:${MONGO_ARBITER_PORT}', arbiterOnly: true }
   ]
 })"
 
